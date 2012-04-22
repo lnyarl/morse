13c4ef79a705ba3e571cea9ace00ca448468fd19
Yongjae Choi <lnyarl@gmail.com>
2012-04-23 00:27:35 +0900

    sample added

---
+++ sample/sample1.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	var morse = new Morse();
+	
+	morse.bind($('#input'));
+});
---
+++ sample/sample2.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var morse = new Morse();
+	var result = $('#result');
+	var letter_out = $('#letter');
+	var morse_out = $('#morse');
+	var error_out = $('#error');
+	
+	morse.input_handler = function(input){ morse_out.text(morse_out.text() + input); }
+	morse.letter_handler = function(input){ letter_out.text(letter_out.text() + Morse.get(input)); }
+	morse.word_handler = function(word){  result.text(result.text() + morse.out()); }
+	morse.error_handler = function(e){ error_out.html(e); }
+	morse.bind($('#mydiv'));
+});

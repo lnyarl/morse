d76227bd523da51b8b63e27a76dae5ecd72a160c
Yongjae Choi <lnyarl@gmail.com>
2012-04-24 01:22:56 +0900

    index page update

--- page/js/script.js
+++ page/js/script.js
@@ -3,7 +3,7 @@
 */
 
 $(document).ready(function(){
-	var morse = new Morse();
+	var morse = new Morse(150);
 	var input_element = $('#input');
 	var result_element = $('#result');
 
@@ -24,7 +24,48 @@ $(document).ready(function(){
 
 	var handler = morse.getHandler();
 	$('#input').keydown(handler.on).keyup(handler.off);
+
+	make_code_table(Morse.table);
 });
+
+function make_code_table(code_list) {
+	var template = $('.template').clone().removeClass('template').removeClass('invisible');
+	var table = $('#code-table tbody');
+
+	$('.template').remove();
+
+	var j = 1;
+	var max_row = 9;
+	var append = function(record) {
+		if(table.find('tr').length < j) {
+			table.append(record);
+		} else {
+			table.find('tr:nth-child('+j+')').append(record.find('td'));
+		}
+	}
+	for(i in code_list) {
+		var record = template.clone();
+		record.find('.letter').text(code_list[i]);
+		record.find('.code').text(i);
+
+		if(j < max_row) {
+			append(record);
+			j++;
+		} else {
+			var tr = $('#code-table thead tr');
+			var letter_head = tr.find('.letter:first').clone();
+			var code_head = tr.find('.code:first').clone();
+			tr.append(letter_head).append(code_head);
+			append(record);
+			j=1;
+		}
+	}
+
+	for(; j <= max_row; j++) {
+		var record = template.clone();
+		table.find('tr:nth-child('+j+')').append(record.find('td'));
+	}
+}
 
 
 

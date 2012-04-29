8099e177719086ff283e3b5f0ec6a8e97fef968d
Yongjae Choi <lnyarl@gmail.com>
2012-04-29 22:24:17 +0900

    'tweet this' added

--- page/js/script.js
+++ page/js/script.js
@@ -6,15 +6,22 @@ $(document).ready(function(){
 	var default_dot_duration = 150;
 	var morse = new Morse(default_dot_duration);
 	var input_element = $('#input');
+	var input_code_element = $('#input-code');
 	var buffer_element = $('#input-buffer');
 	var result_element = $('#result');
 	var table = $('#code-table');
 	var cursor = $('#cursor');
 
-	var append = function(l) {
-	}
+	var code = decode(getQueryVariable('code'));
+
+	var blink = blinkCursor(default_dot_duration);
+
+	make_code_table(Morse.table);
+
+	defaultCode(code, morse);
 
 	morse.setMorseKey(32);
+	morse.setResetKey(27);
 	morse.input_handler = function(input){ 
 		input_element.css('border', '1px solid #000');
 		cursor.before(input); 
@@ -36,6 +43,7 @@ $(document).ready(function(){
 		if(!!correct) {
 			table.find('.' + correct).addClass('correct').removeClass('selected');
 		}
+
 		return false;
 	};
 
@@ -48,6 +56,7 @@ $(document).ready(function(){
 		try {
 			var letter = Morse.get(input);
 			input_element.append(text.substr(0, text.length - input.length ) + letter);
+			input_code_element.append(input + ' ');
 		} catch (e) {
 			input_element.css('border', '2px solid #ff0000');
 			input_element.append(text.substr(0, text.length - input.length ));
@@ -56,18 +65,28 @@ $(document).ready(function(){
 		//result_element.append(morse.out(true)); 
 		buffer_element.text('');
 		input_element.append(cursor);
+		buildTweetURL(input_code_element.text());
 	};
 
 	morse.word_handler = function(input){ 
 		//result_element.append(' '); 
-		cursor.before(" ");
+		cursor.before(' ');
 		buffer_element.text('');
+		input_code_element.append(' ');
+	};
+	
+	morse.reset_handler = function() {
+		buffer_element.text('');
+		input_element.text('').append(cursor);
+		input_code_element.text('');
+		buffer_element.text('');
+		table.find('.selected').removeClass('selected');
+		table.find('.correct').removeClass('correct');
 	};
 
 	var handler = morse.getHandler();
-	$(document).keydown(handler.on).keyup(handler.off);
+	$(document).keydown(handler.on).keyup(handler.off).keyup(handler.reset);
 
-	var blink = blinkCursor(default_dot_duration);
 
 	$(function() {
 		$( "#slider" ).slider({
@@ -89,8 +108,18 @@ $(document).ready(function(){
 	$( "#slider-value" ).text( default_dot_duration + " ms"  );
 
 
-	make_code_table(Morse.table);
 });
+
+function defaultCode(str, morse) {
+	var input_code_element = $('#input-code');
+	input_code_element.append(str);
+	buildTweetURL(str);
+	str = decode(str).split('  ').join('/');
+	
+	morse.feed(str);
+	$('#cursor').before(morse.out(true));
+}
+
 
 function blinkCursor(dot_duration) {
 	return setInterval(function (){
@@ -140,5 +169,54 @@ function make_code_table(code_list) {
 }
 
 
+function buildTweetURL(morse_code) {
+	$('.twitter-share-button').remove();
+	var encoded_code = encode(morse_code).split('.').join('%2E').split('-').join('%2D');
+	var url = 'https://twitter.com/share/?text=' + morse_code + ' %5Bhttp://morse.magae.net%3Fcode%3D' + encode(encoded_code)+ '%5D';
+	var a = $('<a></a>').attr({
+		"href":url,
+		"id":"twitter-share-button-morse",
+		"data-count":"none",
+		"class":"twitter-share-button",
+		"data-lang":"en",
+	});
+	$('#tweet-button').append(a);
+	maketweet(document,"script","twitter-wjs");
+}
 
 
+
+
+function getQueryVariable(variable) { 
+	var query = window.location.search.substring(1); 
+	var vars = query.split("&"); 
+	for (var i=0;i<vars.length;i++) { 
+		var pair = vars[i].split("="); 
+		if (pair[0] == variable) { 
+			return pair[1]; 
+		} 
+	} 
+} 
+function decode(str) {
+	if(!str) return undefined;
+
+	if(!!decodeURIComponent){
+		return decodeURIComponent(str);
+	} else if(!!decodeURI) {
+		return decodeURI(str);
+	} else {
+		return unescape(str);
+	}
+}
+
+function encode(str) {
+	if(!str) return undefined;
+
+	if(!!encodeURIComponent){
+		return encodeURIComponent(str);
+	}else if(!!encodeURI) {
+		return encodeURI(str);
+	} else {
+		return escape(str);
+	}
+}
